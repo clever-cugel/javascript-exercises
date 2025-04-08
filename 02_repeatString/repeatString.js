2674358a8029d3d7e37471305a46dc33df380894
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-08 16:26:18 -0400

    Do 07

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(line, num) {
+    newline = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++){
+        newline += line;
+    }
+    return newline;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(removeArray, ...nums) {
+    let flag = 0;
+    let newArray = [];
+    for (const entry of removeArray) {
+        for (const subentry of nums) {
+            if (entry === subentry) {
+                flag = 1;
+            }
+        }
+        if (flag === 0) {
+            newArray.push(entry);
+        }
+        flag = 0;
+    }
+    return newArray;
 };
 
 // Do not edit below this line
