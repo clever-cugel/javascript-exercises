62d240feaa73ee01c9df55d440868ea1796b4bf9
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-07 22:49:49 -0400

    Finish thru 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+//leap year = divisible by 4
+//but if divisible by 100 not leap year
+//unless ALSO divisible by 400
+    if (year < 100 && year % 4 === 0) {
+        return true;
+    } else if (year < 400 && year % 100 !== 0 && year % 4 === 0) {
+        return true;
+    } else if (year % 100 === 0 && year % 400 === 0 && year % 4 === 0) {
+        return true;
+    } else if (year % 100 !== 0 && year % 4 === 0){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
