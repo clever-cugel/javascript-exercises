e7b5cc7cdfa51d1a2a1eefdf1dfb7b2b840b544c
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-07 20:42:07 -0400

    Finish first 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(enterString) {
+    let reverseString = '';
+    for (let i = enterString.length - 1; i >= 0; i--) {
+        reverseString += enterString[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
