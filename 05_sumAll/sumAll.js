b3840f6c1e05199bd6a0c483e1cf9fd8ed29536a
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-07 22:19:44 -0400

    Finish up to/including 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR';
+    }
 
+    if (num1 - Math.floor(num1) != 0 || num2 - Math.floor(num2) != 0) {
+        return 'ERROR';
+    }
+    
+    let sum = 0;
+    let i = 0;
+    let j = 0;
+    let total = 0;
+    if (num1 < num2) {
+        i = num1;
+        j = num2;
+    } else {
+        i = num2;
+        j = num1;
+    }
+    for (i; i <= j; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
