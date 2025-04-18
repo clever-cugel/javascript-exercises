4347a34c693357a160648de2825f70f44ffc0c35
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-18 17:57:56 -0400

    Do 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, nextItem) => {
+    return total += nextItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, nextItem) => {
+    return total *= nextItem;
+  },1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a;
+	for (let i = 1; i < b; i++) {
+    total *= a;
+  }
+  return total;
+  //there is apparently a Math.pow method I could have just used
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let total = num;
+  if (num === 0) {
+    return 1;
+  } else {
+    for (let i = num - 1; i > 0; i--) {
+      total *= i;
+    }
+    return total;
+  }
 	
 };
 
