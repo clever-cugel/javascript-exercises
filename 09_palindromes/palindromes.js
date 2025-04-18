1c3f74e0cdf72ce9c2a0723bd17e33dad05f237b
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-18 19:59:50 -0400

    Do 09 palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.split('');
+    str = str.filter((n) => n != '!' && n != ' ' && n != '.' && n != ',' && n != ';' && n != ':' && n != '?' && n != '-' && n != '_' && n != '/' && n != "'" && n != '&' && n != '*' && n != '\\');
+    str = str.join('');
 
+    let midex;
+    let leftHalf;
+    let rightHalf;
+
+    if (str.length % 2 === 0) {
+        midex = str.length / 2;
+        leftHalf = str.slice(0, midex);
+        rightHalf = str.slice(midex, str.length)
+    } else {
+        midex = Math.floor(str.length / 2);
+        leftHalf = str.slice(0, midex + 1);
+        rightHalf = str.slice(midex, str.length);
+    }
+    rightHalf = rightHalf.split('');
+    rightHalf = rightHalf.reverse();
+    rightHalf = rightHalf.join('');
+    
+    if (leftHalf.toLowerCase() === rightHalf.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
