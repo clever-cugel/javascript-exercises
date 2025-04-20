f859e79f890889a9808a4c398f07e53052a2f410
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-20 00:09:06 -0400

    Do 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let first = 0;
+    let second = 1;
+    let memory = 0;
+    
+    if (typeof num === 'string') {
+        num = +num;
+    }
 
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else if (num < 0) {
+        return 'OOPS';
+    } else {
+        for (let i = 1; i < num; i++) {
+            memory = second;
+            second = second + first;
+            first = memory;
+        }
+        return second;
+    }
 };
 
 // Do not edit below this line
