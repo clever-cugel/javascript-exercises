67ea6d3874c19df90214dcd0d5aa2cdcb90270f2
clever-cugel <202036048+clever-cugel@users.noreply.github.com>
2025-04-20 01:04:05 -0400

    Do 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let yearsA;
+    let yearsB;
+    let today = 2025;
 
+    let yearOrder = arr.sort((personA, personB) => {
+        if (!(personA.yearOfDeath)) {
+            yearsA = today - personA.yearOfBirth;
+        } else {
+            yearsA = personA.yearOfDeath - personA.yearOfBirth;
+        }
+
+        if (!(personB.yearOfDeath)) {
+            yearsB = today - personB.yearOfBirth;
+        } else {
+            yearsB = personB.yearOfDeath - personB.yearOfBirth;
+        }
+
+        if (yearsA > yearsB) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    
+    return yearOrder[0];
 };
 
 // Do not edit below this line
